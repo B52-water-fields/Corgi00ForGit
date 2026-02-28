07cb4dd86b98bafc027f718e3df6204db9cf82ce
mtosh <mtosh@ToToMainPC>
2026-02-28 11:55:19 +0900

    OKNG音調整

--- src/B00130SoundCreate.java
+++ src/B00130SoundCreate.java
@@ -4,125 +4,151 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class B00130SoundCreate{
+	static double NS   = 0;		//無音
+    
+	static double C   = 32.703;	//ド		
+	static double CS  = 34.648;	//ド#		
+	static double D   = 36.708;	//レ		
+	static double DS  = 38.891;	//レ#		
+	static double E   = 41.203;	//ミ		
+	static double F   = 43.654;	//ファ		
+	static double FS  = 46.249;	//ファ#
+	static double G   = 48.999;	//ソ
+	static double GS  = 51.913;	//ソ#	
+	static double A   = 55.000;	//ラ
+	static double AS  = 58.270;	//ラ#
+	static double B   = 61.735;	//シ
+	
 	public static void OK() {
-		byte[][] OkSound = {
-			Sound(261.626  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(261.626  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(523.251  	,200,0.5),
-			Sound(0  		,200,0.5),
-			Sound(523.251  	,200,0.5)
-			};
-		SoundCall(OkSound);
+		byte[][] Sound = {
+				Sound(C	,5  ,100	,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(C	,5  ,100	,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(NS	,1  ,100	,0.5),
+				Sound(C	,7	,100	,0.5),
+				Sound(E 	,7	,100	,0.8)
+				};
+		SoundCall(Sound);
 	}
 	public static void NG() {
-		byte[][] OkSound = {
-			Sound(261.626  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(261.626  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(130.813  	,200,0.5),
-			Sound(0  		,200,0.5),
-			Sound(130.813  	,800,0.8)
-			};
-		SoundCall(OkSound);
+		byte[][] Sound = {
+				Sound(C	,5  ,100	,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(C	,5  ,100	,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(D	,4  ,100	,0.5),
+				Sound(NS	,1  ,100	,0.5),
+				Sound(C  	,4	,500	,0.8)
+				};
+		SoundCall(Sound);
 	}
+	
 	public static void Alert() {
 		byte[][] Sound = {
-			Sound(261.626  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(261.626  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(0  	,200,0.5),
-			Sound(523.251  	,800,0.5),
-			Sound(0  	,200,0.5),
-			Sound(261.626  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(261.626  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(0  	,200,0.5),
-			Sound(523.251  	,800,0.5)
+				Sound(C	,5  ,100	,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(C	,5  ,100	,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(NS	,1  ,100	,0.5),
+				Sound(C	,6  ,300	,0.5),
+				Sound(NS	,1  ,100	,0.5),
+				Sound(C	,5  ,100	,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(C	,5  ,100	,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(NS	,1  ,100	,0.5),
+				Sound(C	,6  ,300	,0.5),
 		};
 		SoundCall(Sound);
 	}
 	
 	public static void Alert2() {
 		byte[][] Sound = {
-			Sound(329.628  	,800,0.5),
-			Sound(440.000  	,200,0.5),
-			Sound(391.995  	,200,0.5),
-			Sound(0  	,200,0.5),
-			Sound(329.628  	,800,0.5),
-			Sound(440.000  	,200,0.5),
-			Sound(391.995  	,200,0.5)
+				Sound(E	,5  ,400	,0.5),
+				Sound(C	,6  ,100	,0.5),
+				Sound(E	,6  ,100	,0.5),
+				Sound(NS	,1  ,100	,0.5),
+				Sound(E	,5  ,400	,0.5),
+				Sound(C	,6  ,100	,0.5),
+				Sound(E	,6  ,100	,0.5),
 		};
 		SoundCall(Sound);
 	}
 	
 	public static void LVUP() {
 		byte[][] Sound = {
-			Sound(349.228  	,100,0.5),
-			Sound(0  	,100,0.5),
-			Sound(349.228  	,100,0.5),
-			Sound(0  	,100,0.5),
-			Sound(349.228  	,100,0.5),
-			Sound(0  	,100,0.5),
-			Sound(349.228  	,100,0.5),
-			Sound(0  	,100,0.5),
-			Sound(311.127  	,200,0.5),
-			Sound(0  	,100,0.5),
-			Sound(391.995  	,400,0.5),
-			Sound(0  	,200,0.5),
-			Sound(349.228  	,800,0.5)
+				Sound(F	,5 	,50		,0.5),
+				Sound(NS	,1  ,50		,0.5),
+				Sound(F	,5 	,50		,0.5),
+				Sound(NS	,1  ,50		,0.5),
+				Sound(F	,5 	,50		,0.5),
+				Sound(NS	,1  ,50		,0.5),
+				Sound(F	,5 	,50		,0.5),
+				Sound(NS	,1  ,50		,0.5),
+				Sound(E	,5  ,100	,0.5),
+				Sound(NS	,1  ,50		,0.5),
+				Sound(G	,5  ,200	,0.5),
+				Sound(NS	,1  ,50		,0.5),
+				Sound(F	,5  ,400	,0.5)
 		};
 		SoundCall(Sound);
 	}
 	
 	public static void CAT() {
 		byte[][] Sound = {
-			Sound(293.665  	,200,0.5),
-			Sound(261.626  	,200,0.5),
-			Sound(174.614  	,200,0.5),
-			Sound(349.228  	,200,0.5),
-			Sound(349.228  	,200,0.5),
-			Sound(293.665  	,200,0.5),
-			Sound(261.626  	,200,0.5),
-			Sound(174.614  	,200,0.5),
-			Sound(349.228  	,200,0.5),
-			Sound(349.228  	,200,0.5),
-			Sound(293.665  	,200,0.5),
-			Sound(261.626  	,200,0.5),
-			Sound(174.614  	,200,0.5),
-			Sound(349.228  	,200,0.5),
-			Sound(146.832  	,200,0.5),
-			Sound(349.228  	,200,0.5),
-			Sound(130.813  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(329.628  	,200,0.5)
+				Sound(D	,5  ,200	,0.5),
+				Sound(C	,5  ,200	,0.5),
+				Sound(F	,4  ,200	,0.5),
+				Sound(F	,5  ,200	,0.5),
+				Sound(F	,5  ,200	,0.5),
+				Sound(D	,5  ,200	,0.5),
+				Sound(C	,5  ,200	,0.5),
+				Sound(F	,4  ,200	,0.5),
+				Sound(F	,5  ,200	,0.5),
+				Sound(F	,5  ,200	,0.5),
+				Sound(D	,5  ,200	,0.5),
+				Sound(D	,5  ,200	,0.5),
+				Sound(F	,4  ,200	,0.5),
+				Sound(F	,5  ,200	,0.5),
+				Sound(F	,4  ,200	,0.5),
+				Sound(D	,4  ,200	,0.5),
+				Sound(C	,4  ,200	,0.5),
+				Sound(E	,5  ,200	,0.5),
+				Sound(E	,5  ,200	,0.5)
 		};
 		SoundCall(Sound);
 	}
-	
 	public static void Condor(){
 		byte[][] Sound = {
-			Sound(220.000  	,400,0.5),
-			Sound(277.183  	,100,0.5),
-			Sound(293.665  	,200,0.5),
-			Sound(277.183  	,200,0.5),
-			Sound(293.665  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(349.228  	,200,0.5),
-			Sound(329.628  	,200,0.5),
-			Sound(349.228  	,200,0.5),
-			Sound(391.995  	,200,0.5),
-			Sound(493.883  	,100,0.5),
-			Sound(440.000  	,800,0.5),
-			Sound(523.251  	,100,0.5),
-			Sound(440.000  	,800,0.5)
-		};
+				Sound(E	,5  ,400,0.5),
+				Sound(A	,6  ,100,0.5),
+				Sound(GS	,6  ,200,0.5),
+				Sound(A	,6  ,200,0.5),
+				Sound(B	,6  ,200,0.5),
+				Sound(C	,6  ,200,0.5),
+				Sound(B	,6  ,200,0.5),
+				Sound(C	,6  ,200,0.5),
+				Sound(D	,6  ,200,0.5),
+				Sound(E	,6  ,200,0.5),
+				Sound(G	,6  ,100,0.5),
+				Sound(E	,6  ,800,0.5)
+			};
 		SoundCall(Sound);
 	}
+
+	public static void SoundTest() {
+		
+		OK();
+		NG();
+		Alert();
+		Alert2();
+		LVUP();
+		CAT();
+		Condor();
+		
+	}
+	
 	
 	public static void SoundCreate() {
 		//音を鳴らすテスト用
@@ -133,103 +159,92 @@ public class B00130SoundCreate{
         int durationMs800 = 800; // 長さ (ms)
         */
         double volume = 0.5; // 音量 (0.0〜1.0)
-        
-        double NS   = 0;		//無音
-        
-		double C3   = 130.813;	//ド3		
-		double CS3  = 138.591;	//ド#3		
-		double D3   = 146.832;	//レ3		
-		double DS3  = 155.563;	//レ#3		
-		double E3   = 164.814;	//ミ3		
-		double F3   = 174.614;	//ファ3		
-		double FS3  = 184.997;	//ファ#3
-		double G3   = 195.998;	//ソ3
-		double GS3  = 207.652;	//ソ#3	
-		double A3   = 220.000;	//ラ3
-		double AS3  = 233.082;	//ラ#3
-		double B3   = 246.942;	//シ3
-        double C4   = 261.626;	//ド4		
-		double CS4  = 277.183;	//ド#4		
-		double D4	= 293.665;	//レ4
-		double DS4	= 311.127;	//レ#4		
-		double E4	= 329.628;	//ミ4		
-		double F4	= 349.228;	//ファ4		
-		double FS4	= 369.994;	//ファ#4		
-		double G4	= 391.995;	//ソ4		
-		double GS4	= 415.305;	//ソ#4		
-		double A4	= 440.000;	//ラ4		
-		double AS4	= 466.164;	//ラ#4		
-		double B4	= 493.883;	//シ4		
-		double C5	= 523.251;	//ド5
 
-		byte[] Sound200_NS  = Sound(NS  ,200,volume);	//無音
+		byte[] Sound200_NS  = Sound(NS,1  ,200,volume);	//無音
 
-		byte[] Sound200_C3   = Sound(C3  ,200,volume);	//ド3		
-		byte[] Sound200_CS3  = Sound(CS3 ,200,volume);	//ド#3		
-		byte[] Sound200_D3   = Sound(D3  ,200,volume);	//レ3		
-		byte[] Sound200_DS3  = Sound(DS3 ,200,volume);	//レ#3		
-		byte[] Sound200_E3   = Sound(E3  ,200,volume);	//ミ3		
-		byte[] Sound200_F3   = Sound(F3  ,200,volume);	//ファ3		
-		byte[] Sound200_FS3  = Sound(FS3 ,200,volume);	//ファ#3		
-		byte[] Sound200_G3   = Sound(G3  ,200,volume);	//ソ3		
-		byte[] Sound200_GS3  = Sound(GS3 ,200,volume);	//ソ#3	
-		byte[] Sound200_A3   = Sound(A3  ,200,volume);	//ラ3		
-		byte[] Sound200_AS3  = Sound(AS3 ,200,volume);	//ラ#3		
-		byte[] Sound200_B3   = Sound(B3  ,200,volume);	//シ3
-        byte[] Sound200_C4   = Sound(C4  ,200,volume);	//ド4
-		byte[] Sound200_CS4  = Sound(CS4 ,200,volume);	//ド#4
-		byte[] Sound200_D4   = Sound(D4  ,200,volume);	//レ4
-		byte[] Sound200_DS4  = Sound(DS4 ,200,volume);	//レ#4
-		byte[] Sound200_E4   = Sound(E4  ,200,volume);	//ミ4
-		byte[] Sound200_F4   = Sound(F4  ,200,volume);	//ファ4
-		byte[] Sound200_FS4  = Sound(FS4 ,200,volume);	//ファ#4
-		byte[] Sound200_G4   = Sound(G4  ,200,volume);	//ソ4
-		byte[] Sound200_GS4  = Sound(GS4 ,200,volume);	//ソ#4
-		byte[] Sound200_A4   = Sound(A4  ,200,volume);	//ラ4
-		byte[] Sound200_AS4  = Sound(AS4 ,200,volume);	//ラ#4
-		byte[] Sound200_B4   = Sound(B4  ,200,volume);	//シ4
-		byte[] Sound200_C5   = Sound(C5  ,200,volume);	//ド5
+		byte[] Sound200_C3   = Sound(C		,3 ,200,volume);	//ド3		
+		byte[] Sound200_CS3  = Sound(CS	,3 ,200,volume);	//ド#3		
+		byte[] Sound200_D3   = Sound(D		,3 ,200,volume);	//レ3		
+		byte[] Sound200_DS3  = Sound(DS	,3 ,200,volume);	//レ#3		
+		byte[] Sound200_E3   = Sound(E		,3 ,200,volume);	//ミ3		
+		byte[] Sound200_F3   = Sound(F		,3 ,200,volume);	//ファ3		
+		byte[] Sound200_FS3  = Sound(FS	,3 ,200,volume);	//ファ#3		
+		byte[] Sound200_G3   = Sound(G		,3 ,200,volume);	//ソ3		
+		byte[] Sound200_GS3  = Sound(GS	,3 ,200,volume);	//ソ#3	
+		byte[] Sound200_A3   = Sound(A		,3 ,200,volume);	//ラ3		
+		byte[] Sound200_AS3  = Sound(AS	,3 ,200,volume);	//ラ#3		
+		byte[] Sound200_B3   = Sound(B		,3 ,200,volume);	//シ3
+        byte[] Sound200_C4   = Sound(C		,4 ,200,volume);	//ド4
+		byte[] Sound200_CS4  = Sound(CS	,4 ,200,volume);	//ド#4
+		byte[] Sound200_D4   = Sound(D		,4 ,200,volume);	//レ4
+		byte[] Sound200_DS4  = Sound(DS	,4 ,200,volume);	//レ#4
+		byte[] Sound200_E4   = Sound(E		,4 ,200,volume);	//ミ4
+		byte[] Sound200_F4   = Sound(F		,4 ,200,volume);	//ファ4
+		byte[] Sound200_FS4  = Sound(FS	,4 ,200,volume);	//ファ#4
+		byte[] Sound200_G4   = Sound(G		,4 ,200,volume);	//ソ4
+		byte[] Sound200_GS4  = Sound(GS	,4 ,200,volume);	//ソ#4
+		byte[] Sound200_A4   = Sound(A		,4 ,200,volume);	//ラ4
+		byte[] Sound200_AS4  = Sound(AS	,4 ,200,volume);	//ラ#4
+		byte[] Sound200_B4   = Sound(B		,4 ,200,volume);	//シ4
+		byte[] Sound200_C5   = Sound(C		,5 ,200,volume);	//ド5
 		
-		byte[] Sound800_C3   = Sound(C3  ,800,volume);	//ド3		
-		byte[] Sound800_CS3  = Sound(CS3 ,800,volume);	//ド#3		
-		byte[] Sound800_D3   = Sound(D3  ,800,volume);	//レ3		
-		byte[] Sound800_DS3  = Sound(DS3 ,800,volume);	//レ#3		
-		byte[] Sound800_E3   = Sound(E3  ,800,volume);	//ミ3		
-		byte[] Sound800_F3   = Sound(F3  ,800,volume);	//ファ3		
-		byte[] Sound800_FS3  = Sound(FS3 ,800,volume);	//ファ#3		
-		byte[] Sound800_G3   = Sound(G3  ,800,volume);	//ソ3		
-		byte[] Sound800_GS3  = Sound(GS3 ,800,volume);	//ソ#3	
-		byte[] Sound800_A3   = Sound(A3  ,800,volume);	//ラ3		
-		byte[] Sound800_AS3  = Sound(AS3 ,800,volume);	//ラ#3		
-		byte[] Sound800_B3   = Sound(B3  ,800,volume);	//シ3
-		byte[] Sound800_NS   = Sound(NS  ,800,volume);	//無音
-        byte[] Sound800_C4   = Sound(C4  ,800,volume);	//ド4
-		byte[] Sound800_CS4  = Sound(CS4 ,800,volume);	//ド#4
-		byte[] Sound800_D4   = Sound(D4  ,800,volume);	//レ4
-		byte[] Sound800_DS4  = Sound(DS4 ,800,volume);	//レ#4
-		byte[] Sound800_E4   = Sound(E4  ,800,volume);	//ミ4
-		byte[] Sound800_F4   = Sound(F4  ,800,volume);	//ファ4
-		byte[] Sound800_FS4  = Sound(FS4 ,800,volume);	//ファ#4
-		byte[] Sound800_G4   = Sound(G4  ,800,volume);	//ソ4
-		byte[] Sound800_GS4  = Sound(GS4 ,800,volume);	//ソ#4
-		byte[] Sound800_A4   = Sound(A4  ,800,volume);	//ラ4
-		byte[] Sound800_AS4  = Sound(AS4 ,800,volume);	//ラ#4
-		byte[] Sound800_B4   = Sound(B4  ,800,volume);	//シ4
-		byte[] Sound800_C5   = Sound(C5  ,800,volume);	//ド5
+		byte[] Sound800_C3   = Sound(C		,3 ,800,volume);	//ド3		
+		byte[] Sound800_CS3  = Sound(CS	,3 ,800,volume);	//ド#3		
+		byte[] Sound800_D3   = Sound(D		,3 ,800,volume);	//レ3		
+		byte[] Sound800_DS3  = Sound(DS	,3 ,800,volume);	//レ#3		
+		byte[] Sound800_E3   = Sound(E		,3 ,800,volume);	//ミ3		
+		byte[] Sound800_F3   = Sound(F		,3 ,800,volume);	//ファ3		
+		byte[] Sound800_FS3  = Sound(FS	,3 ,800,volume);	//ファ#3		
+		byte[] Sound800_G3   = Sound(G		,3 ,800,volume);	//ソ3		
+		byte[] Sound800_GS3  = Sound(GS	,3 ,800,volume);	//ソ#3	
+		byte[] Sound800_A3   = Sound(A		,3 ,800,volume);	//ラ3		
+		byte[] Sound800_AS3  = Sound(AS	,3 ,800,volume);	//ラ#3		
+		byte[] Sound800_B3   = Sound(B		,3 ,800,volume);	//シ3
+		byte[] Sound800_NS   = Sound(NS	,1 ,800,volume);	//無音
+        byte[] Sound800_C4   = Sound(C		,4 ,800,volume);	//ド4
+		byte[] Sound800_CS4  = Sound(CS	,4 ,800,volume);	//ド#4
+		byte[] Sound800_D4   = Sound(D		,4 ,800,volume);	//レ4
+		byte[] Sound800_DS4  = Sound(DS	,4 ,800,volume);	//レ#4
+		byte[] Sound800_E4   = Sound(E		,4 ,800,volume);	//ミ4
+		byte[] Sound800_F4   = Sound(F		,4 ,800,volume);	//ファ4
+		byte[] Sound800_FS4  = Sound(FS	,4 ,800,volume);	//ファ#4
+		byte[] Sound800_G4   = Sound(G		,4 ,800,volume);	//ソ4
+		byte[] Sound800_GS4  = Sound(GS	,4 ,800,volume);	//ソ#4
+		byte[] Sound800_A4   = Sound(A		,4 ,800,volume);	//ラ4
+		byte[] Sound800_AS4  = Sound(AS	,4 ,800,volume);	//ラ#4
+		byte[] Sound800_B4   = Sound(B		,4 ,800,volume);	//シ4
+		byte[] Sound800_C5   = Sound(C		,5 ,800,volume);	//ド5
 		
 		byte[][] Cat = {Sound200_D4,Sound200_C4,Sound200_F3,Sound200_F4,Sound200_F4,Sound200_D4,Sound200_C4,Sound200_F3,Sound200_F4,Sound200_F4};
 		SoundCall(Cat);
 	}
 	
-	private static byte[] Sound(double frequency,int durationMs,double volume) {
-		// オーディオフォーマット設定
-        // 波形生成（サイン波）
-        float MSec = ((float)44.1*durationMs);
-        byte[] buffer = new byte[(int)MSec];
-        for (int i = 0; i < buffer.length; i++) {
-            double angle = (2.0 * Math.PI * i * frequency) / 44100;
-            buffer[i] = (byte) (Math.sin(angle) * 127.0 * volume);
-        }
+	private static byte[] Sound(double frequency,int num,int durationMs,double volume) {
+		int sampleRate = 44100;
+	    int samples = (int)((durationMs / 1000.0) * sampleRate);
+	    int fadeSamples = (int)(sampleRate * 0.003); // 3ms
+	    byte[] buffer = new byte[samples * 2]; // 16bit = 2bytes
+	    
+	    
+	    frequency = frequency*Math.pow(2, num);
+	    
+
+	    for (int i = 0; i < samples; i++) {
+	        double angle = 2.0 * Math.PI * i * frequency / sampleRate;
+	        double env = 1.0;
+	        if (i < fadeSamples) {
+	            env = (double)i / fadeSamples;
+	        }
+
+	        else if (i > samples - fadeSamples) {
+	            env = (double)(samples - i) / fadeSamples;
+	        }
+	        
+	        short value = (short)(Math.sin(angle) * Short.MAX_VALUE * volume * env);
+
+	        buffer[i * 2]     = (byte)(value & 0xff);
+	        buffer[i * 2 + 1] = (byte)((value >> 8) & 0xff);
+	    }
 		return buffer;
 	}
 	private static void SoundCall(byte[][] bufferList){
