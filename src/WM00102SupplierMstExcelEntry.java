47c05fac196ff1d34e4b74979249f93609822671
mizuno <mizuno@192.168.1.224>
2026-04-24 15:14:57 +0900

    202404241514

--- src/WM00102SupplierMstExcelEntry.java
+++ src/WM00102SupplierMstExcelEntry.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
@@ -5,6 +6,11 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableColumnModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
 public class WM00102SupplierMstExcelEntry{
 	static int SetX;
@@ -79,6 +85,93 @@ public class WM00102SupplierMstExcelEntry{
 		main_fm.add(userinfo);
 		main_fm.add(exit_btn);
 		main_fm.add(entry_btn);
+		
+		Object[][] NeedCol = {
+				 {"担当倉庫"				, 1	, 0}
+				,{"荷主CD"					, 1	, 1}
+				,{"仕入先コード"			, 1	, 2}
+				,{"仕入先名1"				, 1	, 3}
+				,{"仕入先名2"				, 1	, 4}
+				,{"仕入先名3"				, 1	, 5}
+				,{"仕入先郵便"				, 1	, 6}
+				,{"仕入先住所1"				, 1	, 7}
+				,{"仕入先住所2"				, 1	, 8}
+				,{"仕入先住所3"				, 1	, 9}
+				,{"仕入先電話"				, 1	,10}
+				,{"仕入先FAX"				, 1	,11}
+				,{"仕入先MAIL"				, 1	,12}
+				,{"コメント1"				, 1	,13}
+				,{"コメント2"				, 1	,14}
+				,{"コメント3"				, 1	,15}
+				,{"基幹Sysコード（部門）"	, 1	,16}
+				,{"基幹Sysコード（荷主）"	, 1	,17}
+				,{"支払いサイト（月数）"	, 0	,18}
+				,{"支払日（末日＝99）"		, 0	,19}
+				,{"締め日（末日＝99）"		, 0	,20}
+				,{"納品先コード"			, 1	,21}
+				,{"部署CD"					, 1	,22}
+				,{"納品先名1"				, 1	,23}
+				};	//フィールド名,フィールドタイプ(0:数値 1:文字列 2:日付時刻),基本のカラム(ゼロスタート),基本のカラム位置※カラム位置は後で読み込んだエクセルの1行目でフィールド名比較して更新されます
+		
+		JLabel LB_SheetList	= B00110FrameParts.JLabelSet(	10, 40,540,20,"以下のデータを登録しようとしています",11,0);
+		main_fm.add(LB_SheetList);
+		
+		String[] columnNames01 = new String[NeedCol.length+1];
+		columnNames01[0] = "Fg";
+		for(int i=0;i<NeedCol.length;i++) {columnNames01[i+1] = (String)NeedCol[i][0];}
+		
+		//編集可能カラムの指定
+		B10010TableControl.RenewTgt = new int[1];
+		B10010TableControl.RenewTgt[0] = 0;
+
+		final DefaultTableModel tableModel_ms01 = new B10010TableControl.MyTableModel01(columnNames01,0);
+		
+		final JTable tb01 = new JTable(tableModel_ms01);
+		tb01.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		tb01.setRowHeight(20*A00000Main.Mul/A00000Main.Div);
+		tb01.setFont(new Font(A00000Main.DefaultFont, Font.PLAIN, 12*A00000Main.Mul/A00000Main.Div));
+		
+		DefaultTableColumnModel columnModel01
+		= (DefaultTableColumnModel)tb01.getColumnModel();
+		
+		//列幅初期設定 表示位置設定
+		TableColumn column = null;
+		column = columnModel01.getColumn( 0);	column.setPreferredWidth( 30*A00000Main.Mul/A00000Main.Div);
+		for(int i=1;i<NeedCol.length;i++) {
+			if(0==(int)NeedCol[i][1]) {
+				column = columnModel01.getColumn(i+1);	column.setPreferredWidth(100*A00000Main.Mul/A00000Main.Div);	column.setCellRenderer(B00110FrameParts.rightCellRenderer());	
+			}else {
+				column = columnModel01.getColumn(i+1);	column.setPreferredWidth(100*A00000Main.Mul/A00000Main.Div);	column.setCellRenderer(B00110FrameParts.leftCellRenderer());	
+			}
+		}
+		JScrollPane scpn01 = B00110FrameParts.JScrollPaneSet(10,65,700,600,tb01);
+		main_fm.add(scpn01);
+		
+		//ヘッダ行取得⇒フィールド名判定
+		//必要フィールドなければシート選択に戻る
+		Object[][] HeaderRead = B00060ToolsExcellControl.ExcellRead2(TgtFilePath,SheetName,1,0);
+		boolean ErrFg = false;
+		
+		if(null==HeaderRead||0==HeaderRead.length) {
+			ErrFg = true;
+		}else {
+			for(int i01=0;i01<NeedCol.length;i01++) {
+				boolean UnHitFg = true;
+				
+				for(int i02=0;i02<HeaderRead[0].length;i02++) {
+					if(((String)NeedCol[i01][0]).equals(""+HeaderRead[0][i02])) {
+						UnHitFg = false;
+						NeedCol[i01][2] = i02;
+						
+						i02=1+HeaderRead[0].length;
+					}
+				}
+				if(UnHitFg) {
+					ErrFg = true;
+				}
+			}
+		}
+		
 		
 		main_fm.setVisible(true);
 		RenewFg = true;
