6baeace0f786f4368c91275ac42b1abb931f7987
mtosh <mtosh@ToToMainPC>
2026-05-16 12:56:41 +0900

    202605161256

--- src/W00030WorkMain.java
+++ src/W00030WorkMain.java
@@ -4,6 +4,7 @@ import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class W00030WorkMain{
 	static int SetX;
@@ -15,16 +16,43 @@ public class W00030WorkMain{
 		if(x==0) {x=SetX;}
 		if(y==0) {y=SetY;}
 
-		final JFrame main_fm = B00110FrameParts.FrameCreate(x,y,700,750,"Corgi00業務メニュー","");
+		final JFrame main_fm = B00110FrameParts.FrameCreate(x,y,820,750,"Corgi00業務メニュー","");
 		
 		JLabel userinfo = B00110FrameParts.UserInfo();
 		JButton exit_btn = B00110FrameParts.ExitBtn();
-		
-
-
 		main_fm.add(userinfo);
 		main_fm.add(exit_btn);
+		
+		//入荷パネル
+		JPanel PN_AboutArrival 	= B00110FrameParts.JPanelSet(		 10, 40,380,235,"NK");
+		JLabel LB_AboutArrival 	= B00110FrameParts.JLabelSet(		 10,  0,150,20,"入荷メニュー",11,0);
+		PN_AboutArrival.add(LB_AboutArrival);
+		
+		//出荷パネル
+		JPanel PN_AboutDeliveryPlan 	= B00110FrameParts.JPanelSet(		410, 40,380,235,"SPPlan");
+		JLabel LB_AboutDeliveryPlan 	= B00110FrameParts.JLabelSet(		 10,  0,150,20,"出荷予定メニュー",11,0);
+		PN_AboutDeliveryPlan.add(LB_AboutDeliveryPlan);
+		
+		//入荷予定検索
+		JButton ArrivalPlanSearch = B00110FrameParts.BtnSet(	 20, 25,120,20,"入荷予定",11);
+		PN_AboutArrival.add(ArrivalPlanSearch);
+
+		main_fm.add(PN_AboutArrival);
+		main_fm.add(PN_AboutDeliveryPlan);
+		
 		main_fm.setVisible(true);
+		
+		//入荷予定検索
+		ArrivalPlanSearch.addActionListener(new AbstractAction(){
+			public void actionPerformed(ActionEvent e){
+				SetX=main_fm.getX();
+				SetY=main_fm.getY();
+
+				main_fm.setVisible(false);
+				main_fm.dispose();
+				WT0001000ArrivalPlanSearch.ArrivalPlanSearch(0,0);
+			}
+		});
 
 		//EXITボタン押下時の挙動
 		exit_btn.addActionListener(new AbstractAction(){
