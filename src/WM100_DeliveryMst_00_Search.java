3a2a1953c33a72d9c546e0d940eb976b5f0533c3
mtosh <mtosh@ToToMainPC>
2026-08-24 09:12:18 +0900

    20208240912

--- src/WM100_DeliveryMst_00_Search.java
+++ src/WM100_DeliveryMst_00_Search.java
@@ -108,13 +108,13 @@ public class WM100_DeliveryMst_00_Search{
 		
 		JLabel LB2_SearchDECD			= B100_FrameParts.JLabelSet(200, 25, 50,20,B100_DefaultVariable.SearchExact		,11,0);
 		JLabel LB2_SearchDepartmentCd	= B100_FrameParts.JLabelSet(200, 50, 50,20,B100_DefaultVariable.SearchExact		,11,0);
-		JLabel LB2_SearchDEName			= B100_FrameParts.JLabelSet(200, 75, 50,20,B100_DefaultVariable.SearchPartial		,11,0);
+		JLabel LB2_SearchDEName			= B100_FrameParts.JLabelSet(200, 75, 50,20,B100_DefaultVariable.SearchPartial	,11,0);
 		JLabel LB2_SearchPost			= B100_FrameParts.JLabelSet(200,100, 50,20,B100_DefaultVariable.SearchPrefix		,11,0);
-		JLabel LB2_SearchAdd			= B100_FrameParts.JLabelSet(200,125, 50,20,B100_DefaultVariable.SearchPartial		,11,0);
-		JLabel LB2_SearchTel			= B100_FrameParts.JLabelSet(450, 25, 50,20,B100_DefaultVariable.SearchPartial		,11,0);
-		JLabel LB2_SearchFax			= B100_FrameParts.JLabelSet(450, 50, 50,20,B100_DefaultVariable.SearchPartial		,11,0);
-		JLabel LB2_SearchMail			= B100_FrameParts.JLabelSet(450, 75, 50,20,B100_DefaultVariable.SearchPartial		,11,0);
-		JLabel LB2_SearchCom			= B100_FrameParts.JLabelSet(450,100, 50,20,B100_DefaultVariable.SearchPartial		,11,0);
+		JLabel LB2_SearchAdd			= B100_FrameParts.JLabelSet(200,125, 50,20,B100_DefaultVariable.SearchPartial	,11,0);
+		JLabel LB2_SearchTel			= B100_FrameParts.JLabelSet(450, 25, 50,20,B100_DefaultVariable.SearchPartial	,11,0);
+		JLabel LB2_SearchFax			= B100_FrameParts.JLabelSet(450, 50, 50,20,B100_DefaultVariable.SearchPartial	,11,0);
+		JLabel LB2_SearchMail			= B100_FrameParts.JLabelSet(450, 75, 50,20,B100_DefaultVariable.SearchPartial	,11,0);
+		JLabel LB2_SearchCom			= B100_FrameParts.JLabelSet(450,100, 50,20,B100_DefaultVariable.SearchPartial	,11,0);
 		JLabel LB2_SearchPrefecturesCd	= B100_FrameParts.JLabelSet(700, 25, 50,20,""				,11,0);
 		JLabel LB2_SearchMunicipalityCd	= B100_FrameParts.JLabelSet(800, 50, 50,20,""				,11,0);
 		JLabel LB2_SearchDelFg			= B100_FrameParts.JLabelSet(700, 75, 50,20,""				,11,0);
